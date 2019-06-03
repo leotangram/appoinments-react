389696b4d80790a4d7b85373446da4e0413203fb
Leonardo Omaña <leotangram@gmail.com>
2019-06-03 17:00:14 -0500

    useState.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 function App() {
+  // useState retorna 2 funciones
+  // El state actual = this.state
+  // Función que actualiza el state = this.setState({})
+  const [appointment, setAppointment] = useState()
+  console.log(appointment)
   return (
     <h1>Hola</h1>
-  );
+  )
 }
 
-export default App;
+export default App
